116dea8a19b3b9d1cb05efaa1bf4ed22297e8a6a
Mila07 <llo_07@mail.ru>
2019-06-28 23:33:39 +0300

    исправила

--- hv02_easy1.py
+++ hv02_easy1.py
@@ -1,5 +1,6 @@
 fruits = ['Яблоко', 'Банан', 'Киви', 'Виноград', 'Груша',]
 last_name = len(fruits)
-i=0
+i = 0
 for i in range(last_name):
-   print(str(i + 1) + '.' + '{}'.format(fruits[i]))
+
+    print(str(i + 1) + '.' + '{: >10}'.format(fruits[i]))
